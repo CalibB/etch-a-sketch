adbcbfd96efa33000dd576f6e3196fa7c9f2f79c
CalibB <110089911+CalibB@users.noreply.github.com>
2023-01-06 17:18:39 +0200

    feat: Added an event listener for when user mouses over a square it changes to a random rgb colour

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -19,3 +19,19 @@ for (let i = 1; i <= gridDimensions; i++) {
 function randomNum() {
     return (Math.floor(Math.random() * 100));
 };
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', (e) => {
+        let randoNum1 = randomNum();
+        let randoNum2 = randomNum();
+        let randoNum3 = randomNum();
+
+        if (e.ctrlKey) {
+            // Do nothing as a way for better improvement on the grid
+        } else {
+            e.target.style.backgroundColor = `rgb(${randoNum1}, ${randoNum2}, ${randoNum3})`;
+        }  
+    })
+});
