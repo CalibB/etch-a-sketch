6480d9fb486a481dfae9afc53e5392c672022e89
CalibB <110089911+CalibB@users.noreply.github.com>
2023-01-06 17:55:44 +0200

    feat: added functionality for clearing the grid

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -43,3 +43,35 @@ clearBtn.addEventListener('click', () => {
         square.style.backgroundColor = '#EAFDFC';
     })
 });
+
+// Change Grid functionality
+
+const changeGridBtn = document.querySelector('.custom__btn');
+
+changeGridBtn.addEventListener('click', () => {
+    let newDimensions = Number(prompt("Enter a number for your new dimensions up to 100."));
+
+    if (newDimensions > 100) {
+        newDimensions = Number(prompt('Please enter a number less than 100.'));
+    } else {
+
+        // Remove existing grid
+
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
+        };
+
+        // Set new grid
+
+        gridDimensions = newDimensions * newDimensions;
+
+        grid.style.gridTemplateColumns = `repeat(${newDimensions}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${newDimensions}, 1fr)`;
+
+        for (let i = 1; i <= gridDimensions; i++) {
+            const square = document.createElement('div');
+            square.classList.add("square");
+            grid.appendChild(square);
+        }
+    }
+})
